923e8b88e7fe87869a0eb8b7ac60e04f8afca607
joaodaniel2001 <joaodaniel2al@gmail.com>
2025-08-13 12:23:41 -0300

    13.08.2025

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    sobrenome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha = db.Column(db.String(60), nullable=False)
